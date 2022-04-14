eaf0308aff9a957345b9a491ae28b8d880740cf6
imzikra <ramzaa.zikra@gmail.com>
2022-04-14 19:51:33 +0530

    1 file modified

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -10,7 +10,7 @@ import { Album } from '../album';
   styleUrls: ['./product-tracklisting.component.css'],
 })
 export class ProductTracklistingComponent implements OnInit {
-  albumInfo : Albu;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) {}
 
